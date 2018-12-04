269d5d9c7f6f0d935f3d78d61d5bbc3f1a972a77
ojka91 <ojkalp@gmail.com>
2018-12-04 16:58:51 +0100

    solved a problem with the navbar after merge

--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@ var myApp = new Vue({
     el: '#app',
     data: {
         page: 'home',
-        show: true,
+      
 
     },
     methods: {
         swap: function (id) {
             this.page = id;
+            var show = document.querySelector(".show");
+            if(show){
+                show.classList.remove("show");
+            }
+            
         },
+      
     }
 });
