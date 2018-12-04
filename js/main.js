d728ade6c56f70c7129a0303a2c28c4be2433772
ojka91 <ojkalp@gmail.com>
2018-12-04 16:01:32 +0100

    added merged html to test the merge of the SPA

--- js/main.js
+++ js/main.js
@@ -1,96 +1,13 @@
-var data = [
-    {
-        "name": "Damian",
-        "last_name": "Quintero",
-        "age": 22,
-        "country": "Spain",
-        "country_id": "SP",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "10:30",
-        "oponent": "Abdul Wainwright"
-     },
-    {
-        "name": "Abdul",
-        "last_name": "Wainwright",
-        "age": 27,
-        "country": "Morocco",
-        "country_id": "MA",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "10:30",
-        "oponent": "Damian Quintero"
-     },
-    {
-        "name": "Luciano",
-        "last_name": "Snider",
-        "age": 20,
-        "country": "England",
-        "country_id": "EN",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "9:30",
-        "oponent": "Benn Cornish"
-     },
-    {
-        "name": "Benn",
-        "last_name": "Cornish",
-        "age": 23,
-        "country": "United States",
-        "country_id": "USA",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "9:30",
-        "oponent": "Luciano Snider"
-     },
-    {
-        "name": "Anayah",
-        "last_name": "Long",
-        "age": 22,
-        "country": "Finland",
-        "country_id": "FI",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "10:00",
-        "oponent": "Patrik Warner"
-     },
-    {
-        "name": "Patrick",
-        "last_name": "Warner",
-        "age": 28,
-        "country": "Germany",
-        "country_id": "GER",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "10:00",
-        "oponent": "Anayah Long"
-     },
-      {
-        "name": "Lee",
-        "last_name": "An",
-        "age": 25,
-        "country": "China",
-        "country_id": "CN",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "11:00",
-        "oponent": "Willem Mendoza"
-     },
-    {
-        "name": "Willem",
-        "last_name": "Mendoza",
-        "age": 20,
-        "country": "Colombia",
-        "country_id": "COL",
-        "style": "Kata",
-        "tatami": 1,
-        "time": "11:00",
-        "oponent": "Lee An"
-     },
+var myApp = new Vue({
+    el: '#app',
+    data: {
+        page: 'home',
+        show: true,
 
-
-
-
-
-
-]
+    },
+    methods: {
+        swap: function (id) {
+            this.page = id;
+        },
+    }
+});
