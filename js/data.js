6975a8e0d3d41f490d67fcebc211bc705a20bda2
ojka91 <ojkalp@gmail.com>
2018-12-05 15:07:02 +0100

    index is now the merged file. merged done succeful. json data finished

--- js/data.js
+++ js/data.js
@@ -87,6 +87,272 @@ var data = [
         "time": "11:00",
         "oponent": "Lee An"
      },
+    {
+        "name": "Rajveer",
+        "last_name": "Mayo",
+        "age": 28,
+        "country": "Afghanistan",
+        "country_id": "AF",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "11:30",
+        "oponent": "Renae Lu"
+     },
+      {
+        "name": "Renae",
+        "last_name": "Lu",
+        "age": 23,
+        "country": "France",
+        "country_id": "FR",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "11:30",
+        "oponent": "Rajveer Mayo"
+     },
+      {
+        "name": "Phillips",
+        "last_name": "Shanelle",
+        "age": 26,
+        "country": "United States",
+        "country_id": "USA",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "12:00",
+        "oponent": "Haiden Bull"
+     },
+      {
+        "name": "Haiden",
+        "last_name": "Bull",
+        "age": 29,
+        "country": "Norway",
+        "country_id": "NO",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "12:00",
+        "oponent": "Phillips Shanelle"
+     },
+          {
+        "name": "Kiran",
+        "last_name": "Van Achteren",
+        "age": 21,
+        "country": "Germany",
+        "country_id": "GER",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "12:30",
+        "oponent": "Barnabé Kendrick"
+     },
+          {
+        "name": "Bernabé",
+        "last_name": "Kendrick",
+        "age": 21,
+        "country": "Denmark",
+        "country_id": "DEN",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "12:30",
+        "oponent": "Kiran Van Achteren"
+     },
+          {
+        "name": "Dobroslav",
+        "last_name": "Maolain",
+        "age": 23,
+        "country": "Russia",
+        "country_id": "RUS",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "13:00",
+        "oponent": "Aesop Brady"
+     },
+         {
+        "name": "Aesop",
+        "last_name": "Brady",
+        "age": 27,
+        "country": "Philippines",
+        "country_id": "PH",
+        "style": "Kata",
+        "tatami": 1,
+        "time": "13:00",
+        "oponent": "Dobroslav Maolain"
+     },
+        {
+        "name": "Rafael",
+        "last_name": "Aghayev",
+        "age": 27,
+        "country": "Azerbaijan",
+        "country_id": "AZ",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "9:30",
+        "oponent": "Walther Rocco"
+     },
+      {
+        "name": "Walther",
+        "last_name": "Rocco",
+        "age": 22,
+        "country": "Puerto Rico",
+        "country_id": "PR",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "9:30",
+        "oponent": "Rafael Aghayev"
+     },
+          {
+        "name": "Imen",
+        "last_name": "Kempf",
+        "age": 29,
+        "country": "Israel",
+        "country_id": "IL",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "10:00",
+        "oponent": "Dumuzi Dimov"
+     },
+       {
+        "name": "Dumuzi",
+        "last_name": "Dimov",
+        "age": 23,
+        "country": "Ukraine",
+        "country_id": "UA",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "10:00",
+        "oponent": "Imen Kempf"
+     },
+      {
+        "name": "Tessouat",
+        "last_name": "Albinson",
+        "age": 20,
+        "country": "Netherlands",
+        "country_id": "AN",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "10:30",
+        "oponent": "Juozas Goebel"
+     },
+      {
+        "name": "Juozas",
+        "last_name": "Goebel",
+        "age": 27,
+        "country": "South Africa",
+        "country_id": "ZA",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "10:30",
+        "oponent": "Tessouat Albinson"
+     },
+      {
+        "name": "Rory",
+        "last_name": "Kelly",
+        "age": 27,
+        "country": "Ireland",
+        "country_id": "IRL",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "11:00",
+        "oponent": "Pòl Alamilla"
+     },
+       {
+        "name": "Pol",
+        "last_name": "Alamilla",
+        "age": 24,
+        "country": "CATALUNYA FTW",
+        "country_id": "CAT",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "11:00",
+        "oponent": "Rory Kelly"
+     },
+          {
+        "name": "Kenji",
+        "last_name": "Fukui",
+        "age": 27,
+        "country": "Japan",
+        "country_id": "JP",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "11:30",
+        "oponent": "Kaliani Moana"
+     },
+       {
+        "name": "Kaliani",
+        "last_name": "Moana",
+        "age": 23,
+        "country": "Hawaii",
+        "country_id": "HI",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "11:30",
+        "oponent": "Kenji Fukui"
+     },
+      {
+        "name": "Jarred",
+        "last_name": "Farmer",
+        "age": 22,
+        "country": "France",
+        "country_id": "FR",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "12:00",
+        "oponent": "Josiah Fellows"
+     },
+      {
+        "name": "Josiah",
+        "last_name": "Fellows",
+        "age": 22,
+        "country": "Sweden",
+        "country_id": "SE",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "12:00",
+        "oponent": "Jarred Farmer"
+     },
+       {
+        "name": "Ewen",
+        "last_name": "Harrington",
+        "age": 24,
+        "country": "Germany",
+        "country_id": "GER",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "1  2:30",
+        "oponent": "Malika Pacheco"
+     },
+       {
+        "name": "Malika",
+        "last_name": "Pacheco",
+        "age": 30,
+        "country": "Costa Rica",
+        "country_id": "CR",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "12:30",
+        "oponent": "Ewen Harrington"
+     },
+        {
+        "name": "Jozef",
+        "last_name": "Ottilie",
+        "age": 20,
+        "country": "Georgia",
+        "country_id": "GA",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "13:00",
+        "oponent": "Eathan Ferry"
+     },
+      {
+        "name": "Eathan",
+        "last_name": "Ferry",
+        "age": 27,
+        "country": "Portugal",
+        "country_id": "PT",
+        "style": "Kumite",
+        "tatami": 1,
+        "time": "13:00",
+        "oponent": "Jozef Ottilie"
+     },
+
+
 
 
 ]
