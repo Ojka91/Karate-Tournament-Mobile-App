d2f5f545bb74d98438b7d6969300cc9c51309081
ojka91 <ojkalp@gmail.com>
2018-12-12 17:04:29 +0100

    added chat functionality

--- js/main.js
+++ js/main.js
@@ -7,34 +7,93 @@ var myApp = new Vue({
         filteredTeams: myTeams,
         location: '',
         direction: '',
-        map:'',
+        map: '',
         participants: data.participants,
         classPre: data.classification[0].pre,
-        descriptions:'',
-        name:'',
+        descriptions: '',
+        name: '',
     },
-    computed:{
-        filters2(){
+    computed: {
+        filters2() {
             return this.teams.filter(team => {
                 var ku = document.getElementById("kumite").checked && team.kumite.name;
                 var ka = document.getElementById("kataf").checked && team.kata.name;
-                
+
                 return ku || ka;
             })
         }
     },
     methods: {
-        
-        description: function (des){
+        //display the description of the players
+        description: function (des) {
             this.name = des;
-            for(var x = 0; x<data.participants.length; x++){
-                
-                if (this.name.match(data.participants[x].name) ){
+            for (var x = 0; x < data.participants.length; x++) {
+
+                if (this.name.match(data.participants[x].name)) {
                     this.descriptions = data.participants[x].description;
                 }
             }
         },
-        
+
+        //write the post to firebase database
+        writeNewPost: function () {
+
+            // https://firebase.google.com/docs/database/web/read-and-write
+
+            var textToSend =  document.getElementById("textInput").value;
+            // Values
+            var message = {
+                message: textToSend,
+                name: firebase.auth().currentUser.displayName,
+            }
+            console.log(message);
+
+
+            firebase.database().ref('chat-karate-tournament').push(message)
+            // A post entry.
+
+            // Get a key for a new Post.
+
+            //Write data+
+
+            console.log("write");
+            this.getPosts()
+        },
+
+        //get posts from firebase database and print them 
+        getPosts: function () {
+
+            firebase.database().ref('chat-karate-tournament').on('value', function (data) {
+                var posts = document.getElementById("posts");
+                posts.innerHTML = "";
+
+                var messages = data.val();
+
+                for (var key in messages) {
+                    var text = document.createElement("div");
+                    var element = messages[key];
+
+                    text.append(firebase.auth().currentUser.displayName + ": " +element.message);
+                    posts.append(text);
+                }
+
+            })
+
+            console.log("getting posts");
+
+        },
+
+        //create the login function and swap to chat webpage
+        login: function () {
+            // https://firebase.google.com/docs/auth/web/google-signin
+
+            // Provider
+            var provider = new firebase.auth.GoogleAuthProvider();
+            // How to Log In
+            firebase.auth().signInWithPopup(provider);
+            this.swap("chat");
+
+        },
         //function that change pages SPA
         swap: function (id) {
             this.page = id;
@@ -45,45 +104,39 @@ var myApp = new Vue({
                 show.classList.remove("show");
             }
         },
-        
+
         //function that filters teams
         filters: function () {
             this.filteredTeams = []
             for (var x = 0; x < this.teams.length; x++) {
-                
+
                 if (document.getElementById("kumite").checked === false && document.getElementById("kataf").checked === false) {
                     this.filteredTeams.push(this.teams[x]);
-                  
 
-                }
-                else if (document.getElementById("kumite").checked && document.getElementById("kataf").checked) {
+
+                } else if (document.getElementById("kumite").checked && document.getElementById("kataf").checked) {
                     this.filteredTeams.push(this.teams[x]);
 
-                }
+                } else if (document.getElementById("kumite").checked && this.teams[x].kumite.name && document.getElementById("kataf").checked === false) {
 
-                
-                else if (document.getElementById("kumite").checked && this.teams[x].kumite.name && document.getElementById("kataf").checked === false) {
+                    this.filteredTeams.push(this.teams[x]);
 
-                        this.filteredTeams.push(this.teams[x]);
+                    if (this.teams[x].kata) {
 
-                        if (this.teams[x].kata) {
-
-//                            delete this.filteredTeams[this.filteredTeams.length - 1].kata;
-                        }
-                }
-                
-                else if (document.getElementById("kataf").checked && this.teams[x].kata.name && document.getElementById("kumite").checked === false) {
+                        //                            delete this.filteredTeams[this.filteredTeams.length - 1].kata;
+                    }
+                } else if (document.getElementById("kataf").checked && this.teams[x].kata.name && document.getElementById("kumite").checked === false) {
 
                     this.filteredTeams.push(this.teams[x]);
 
                     if (this.teams[x].kumite) {
 
-//                        delete this.filteredTeams[this.filteredTeams.length - 1].kumite;
+                        //                        delete this.filteredTeams[this.filteredTeams.length - 1].kumite;
                     }
                 }
             }
         },
-        
+
     }
 });
 
@@ -103,7 +156,6 @@ myApp.directionName = data.location[0].name;
 
 
 arrayCountries();
-
 
 
 
